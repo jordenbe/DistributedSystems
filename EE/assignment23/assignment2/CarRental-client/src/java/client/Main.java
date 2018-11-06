3a494dee59f0bc5712709f8724194a16c56fabcb
Jorden <jorden.belis@student.kdg.be>
2018-11-06 13:49:30 +0100

    assignment23 EE

---
+++ EE/assignment23/assignment2/CarRental-client/src/java/client/Main.java
@@ -0,0 +1,171 @@
+package client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.EJB;
+import javax.naming.InitialContext;
+import rental.Car;
+import rental.CarType;
+import rental.Reservation;
+import rental.ReservationConstraints;
+import session.CarRentalSessionRemote;
+import session.ManagerSessionRemote;
+
+public class Main extends AbstractTestManagement<CarRentalSessionRemote, ManagerSessionRemote> {
+
+    
+    @EJB
+    private static ManagerSessionRemote ms;
+    
+    public Main(String scriptFile) {
+        super(scriptFile);
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+        Main main = new Main("trips");
+        //ManagerSessionRemote ms = main.getNewManagerSession("main","Brutus");
+        loadRental("hertz.csv",ms);
+        loadRental("dockx.csv",ms);
+        // TODO: use updated manager interface to load cars into companies
+        main.run();
+    }
+
+    @Override
+    protected Set<String> getBestClients(ManagerSessionRemote ms) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected String getCheapestCarType(CarRentalSessionRemote session, Date start, Date end, String region) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected CarType getMostPopularCarTypeIn(ManagerSessionRemote ms, String carRentalCompanyName, int year) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected CarRentalSessionRemote getNewReservationSession(String name) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected ManagerSessionRemote getNewManagerSession(String name, String carRentalName) throws Exception {
+        ManagerSessionRemote ms = (ManagerSessionRemote) new InitialContext().lookup(ManagerSessionRemote.class.getName());
+        return ms;
+    }
+
+    @Override
+    protected void checkForAvailableCarTypes(CarRentalSessionRemote session, Date start, Date end) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void addQuoteToSession(CarRentalSessionRemote session, String name, Date start, Date end, String carType, String region) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected List<Reservation> confirmQuotes(CarRentalSessionRemote session, String name) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected int getNumberOfReservationsBy(ManagerSessionRemote ms, String clientName) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected int getNumberOfReservationsForCarType(ManagerSessionRemote ms, String carRentalName, String carType) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+    
+    
+    /**************************/
+    /* RENTALSTORE LOAD CODE  */
+    /**************************/
+
+    public static void loadRental(String datafile, ManagerSessionRemote ms) {
+        try {
+            CrcData data = loadData(datafile);
+            for(CarType type : data.carTypes)
+                ms.addCarType(type);
+            for(Car car : data.cars)
+                ms.addCar(car);
+            ms.addCarRentalCompany(data.name, data.regions, data.cars);
+            Logger.getLogger(Main.class.getName()).log(Level.INFO, "Loaded {0} from file {1}", new Object[]{data.name, datafile});
+        } catch (NumberFormatException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, "bad file", ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static CrcData loadData(String datafile)
+            throws NumberFormatException, IOException {
+
+        CrcData out = new CrcData();
+        StringTokenizer csvReader;
+        int nextuid = 0;
+       
+        //open file from jar
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(Main.class.getClassLoader().getResourceAsStream(datafile)));
+        
+        try {
+            while (in.ready()) {
+                String line = in.readLine();
+                
+                if (line.startsWith("#")) {
+                    // comment -> skip					
+                } else if (line.startsWith("-")) {
+                    csvReader = new StringTokenizer(line.substring(1), ",");
+                    out.name = csvReader.nextToken();
+                    out.regions = Arrays.asList(csvReader.nextToken().split(":"));
+                } else {
+                    csvReader = new StringTokenizer(line, ",");
+                    //create new car type from first 5 fields
+                    CarType type = new CarType(csvReader.nextToken(),
+                            Integer.parseInt(csvReader.nextToken()),
+                            Float.parseFloat(csvReader.nextToken()),
+                            Double.parseDouble(csvReader.nextToken()),
+                            Boolean.parseBoolean(csvReader.nextToken()));
+                    //create N new cars with given type, where N is the 5th field
+                    out.carTypes.add(type);
+                    for (int i = Integer.parseInt(csvReader.nextToken()); i > 0; i--) {
+                        out.cars.add(new Car(nextuid++, type));
+                    }        
+                }
+            } 
+        } finally {
+            in.close();
+        }
+
+        return out;
+    }
+    
+    static class CrcData {
+            public List<Car> cars = new LinkedList<Car>();
+            public String name;
+            public List<String> regions =  new LinkedList<String>();
+            public List<CarType> carTypes = new LinkedList<CarType>();
+    }
+}
---
+++ EE/assignment23/assignment2/CarRental-lib/src/rental/Reservation.java
@@ -0,0 +1,44 @@
+package rental;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Reservation extends Quote {
+
+    @Id
+    private int carId;
+    
+    /***************
+     * CONSTRUCTOR *
+     ***************/
+    
+    public Reservation() // Default constructor
+    { 
+        super();
+    }
+
+    public Reservation(Quote quote, int carId) {
+    	super(quote.getCarRenter(), quote.getStartDate(), quote.getEndDate(), 
+    		quote.getRentalCompany(), quote.getCarType(), quote.getRentalPrice());
+        this.carId = carId;
+    }
+    
+    /******
+     * ID *
+     ******/
+    
+    public int getCarId() {
+    	return carId;
+    }
+    
+    /*************
+     * TO STRING *
+     *************/
+    
+    @Override
+    public String toString() {
+        return String.format("Reservation for %s from %s to %s at %s\nCar type: %s\tCar: %s\nTotal price: %.2f", 
+                getCarRenter(), getStartDate(), getEndDate(), getRentalCompany(), getCarType(), getCarId(), getRentalPrice());
+    }	
+}
